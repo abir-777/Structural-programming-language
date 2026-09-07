9fc1f42e340b2baef1a9719c86f4175c4d16c2ae
Tanvir Ahmed <162831894+abir-777@users.noreply.github.com>
2026-09-08 00:46:32 +0600

    Add files via upload

---
+++ percantage.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    float m1 = 100;
+    float m2 = 88;
+    float m3 = 92;
+    float m4 = 90;
+    float m5 = 83;
+    float p = (m1+m2+m3+m4+m5)/5;
+    printf("percantage of 5 subjects : %f",p);
+    
+    return 0;
+
+}
